a659b7165bb09a6231c7259831cbc85abd0321e6
KIM-94 <ksw2102@gmail.com>
2021-01-20 00:46:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/lottie2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lottie2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+//import kotlinx.android.synthetic.main.activity_main.*;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
